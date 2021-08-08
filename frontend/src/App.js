c364864e33539bcaddfa42197a2baa93ef65f0ad
gabriel <grocre@gmail.com>
2021-08-08 12:34:00 -0300

    New structure and some React install

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Olá mundo
+        </p> React
+      </header>
+    </div>
+  );
+}
+
+export default App;
