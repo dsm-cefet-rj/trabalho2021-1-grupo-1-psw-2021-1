a698d07b982a195fa499a1d787be7ca021d7fac9
gabriel <grocre@gmail.com>
2021-08-09 23:41:21 -0300

    style changes

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,3 +1,5 @@
+import "../../styles/Card.css"
+
 export default function Card(props) {
 
     return (
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,29 +1,30 @@
+import Header from "../../components/Header"
 import Card from "../../components/Card"
+
 import "../../styles/Home.css"
 
 export default function Home() {
     return (
         <>
+            <Header />
             <header>
                 <a id="logo" href="Index.html">
-                    <img src="./Logo PSW.svg" alt="Logotipo do Tatuando" id="logo"/>
+                    <img src="./Logo PSW.svg" alt="Logotipo do Tatuando" id="logo" />
                 </a>
             </header>
             <section>
                 <div id="search-container">
                     <input type="search" placeholder="Encontre o que procura" />
                 </div>
-                <div id="profile-settings">
-                    <button>Seguindo</button>
-                    <button>Para você</button>
-                </div>
                 <div id="feed-container">
-                    <Card image="./exemplo1.jpg" preco="R$ 500,00"/>
-                    <Card image="./exemplo2.jpg" preco="R$ 100,00"/>
-                    <Card image="./exemplo3.jpg" preco="R$ 350,00"/>
-                    <Card image="./exemplo4.jpg" preco="R$ 300,00"/>
-                    <Card image="./exemplo5.jpg" preco="R$ 270,00"/>
-                    <Card image="./exemplo6.jpg" preco="R$ 800,00"/>
+                    <Card image="./exemplo1.jpg" preco="R$ 500,00" />
+                    <Card image="./exemplo2.jpg" preco="R$ 100,00" />
+                    <Card image="./exemplo3.jpg" preco="R$ 350,00" />
+                    <Card image="./exemplo4.jpg" preco="R$ 300,00" />
+                    <Card image="./exemplo5.jpg" preco="R$ 270,00" />
+                    <Card image="./exemplo6.jpg" preco="R$ 800,00" />
+                    <Card image="./exemplo5.jpg" preco="R$ 270,00" />
+                    <Card image="./exemplo6.jpg" preco="R$ 800,00" />
                 </div>
             </section>
             <nav>
