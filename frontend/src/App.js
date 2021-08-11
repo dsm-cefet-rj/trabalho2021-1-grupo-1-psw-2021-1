d96f9241ea444d30bae5f506dde25447aae37a75
gabriel <grocre@gmail.com>
2021-08-11 12:39:09 -0300

    Routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import ClientProfile from './pages/ClientProfile';
+import Home from "./pages/Home";
+import Payment from './pages/Payment';
+import PaymentConfirm from './pages/PaymentConfirm';
+import ProfessionalProfile from './pages/ProfessionalProfile';
+import SignupClient from './pages/SignupClient';
+import SignupTattoo from './pages/SignupTattoo';
+
 
 
 function App() {
     return ( 
         <Router>
             <Switch>
+                <Route path="/me/paymentId/confirm">
+                    <PaymentConfirm/>
+                </Route>
+                <Route path="/me/paymentId">
+                    <Payment/>
+                </Route>
+                <Route path="/me/newTattoo">
+                    <SignupTattoo/>
+                </Route>
+                <Route path="/tatuadorId">
+                    <ProfessionalProfile/>
+                </Route>
+                <Route path="/signUp">  
+                    <SignupClient/>
+                </Route>
                 <Route path="/me">
                     <ClientProfile/>
                 </Route>
