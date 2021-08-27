787825199cbb65d7ac404b08f2bb1b58a8430faa
Natan Steinbruch <natsteinbruch@gmail.com>
2021-08-27 20:04:33 -0300

    PR Reviews

--- frontend/src/pages/EditTattoo/index.jsx
+++ frontend/src/pages/EditTattoo/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom';
 
 import { api } from '../../services/api';
 
@@ -8,60 +8,56 @@ import "../../styles/EditTattoo.css";
 
 
 export default function EditTattoo() {
-    
-    let [tattoo, setTattoo] = useState({tags:[],preco:""});
-    console.log(tattoo)
+
+    let [tattoo, setTattoo] = useState({ tags: [], preco: "", name: "" });
     const params = useParams();
-    useEffect(() => {
-        async function fetchTattoo (){
-            let {data} = await api.get("/tattoos/?user_id="+params.id_tatuador+"&id="+params.id_tatuagem);
-            console.log(data);
-            setTattoo(data[0])
-        }        
-        fetchTattoo()
-    }, [])
+
+    useEffect(async () => {
+        let { data } = await api.get(`/tattoos/?user_id=${params.id_tatuador}&id=${params.id_tatuagem}`);
+        setTattoo(data[0]);
+    }, []);
 
 
     return (
         <main>
             <form action="#" method="POST" id="form-container">
-                <div class="title">
-                    <input type="text" placeholder={tattoo.name}/>
+                <div className="title">
+                    <input type="text"/>
                 </div>
 
-                <div class="input-container image">
+                <div className="input-container image">
                     <img src={tattoo.image} alt="Imagem da arte da nova tatuagem" />
                     <input type="file" />
                 </div>
 
-                <div class="input-container description">
-                    <label for="description" class="form-content">Descrição:</label>
-                   <div class="content-textarea"> <textarea name="description" placeholder={tattoo.description}></textarea> </div>
+                <div className="input-container description">
+                    <label for="description" className="form-content">Descrição:</label>
+                    <div className="content-textarea"> <textarea name="description">{tattoo.description}</textarea></div>
                 </div>
 
-                <div class="input-container tag">
-                    <label class="form-content">Tags:</label>
-                    <div id="tag-labels">                 
-                    {
-                        tattoo.tags.map(tag => {
+                <div className="input-container tag">
+                    <label className="form-content">Tags:</label>
+                    <div id="tag-labels">
+                        {
+                            tattoo.tags.map(tag => {
                                 return <span> <label>{tag}</label><input type="checkbox" value="on"/></span>
-                            }) 
-                    }     
+                            })
+                        }
                         <button>Adicionar outro</button>
                     </div>
                 </div>
 
-                <div class="input-container price">
+                <div className="input-container price">
                     <label for="price">R$</label>
-                    <input type="number" placeholder={tattoo.preco.substring(3)}/>
+                    <input type="number" placeholder={tattoo.preco.substring(3)} />
                 </div>
 
                 <div id="button-container">
                     <button type="text">
-                        <a href="tatuadorId/">VOLTAR</a>
+                        <Link to={"profile/" + tattoo.user_id}>VOLTAR</Link>
                     </button>
-                    <button type="text" class="submit">
-                        <a href="tatuadorId/">SALVAR</a>
+                    <button type="text" className="submit">
+                        <Link to={"profile/" + tattoo.user_id}>SALVAR</Link>
                     </button>
                 </div>
 
