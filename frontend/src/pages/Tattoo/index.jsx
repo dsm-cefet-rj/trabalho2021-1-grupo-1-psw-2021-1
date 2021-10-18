52c1a5e45a9d55dc456205fb2a511e2f0da257d1
grocre <grocre@gmail.com>
2021-10-18 11:03:27 -0300

    some changes

--- frontend/src/pages/Tattoo/index.jsx
+++ frontend/src/pages/Tattoo/index.jsx
@@ -6,8 +6,12 @@ export default function Tattoo(props) {
 
     const [tattoo, setTattoo] = useState({});
 
-    useEffect( async () => {
-        setTattoo(await (await api.get(`tattoos/${props.match.params.id}`)).data[0]);
+    useEffect(() => { //useEffect nao funciona sempre
+        const axiosGet = async () => {
+            let response = await (api.get(`tattoos/${props.match.params.id}`)).data[0];
+            setTattoo(response);
+        }
+        axiosGet();
     }, []);
 
     console.log(tattoo);
@@ -16,6 +20,11 @@ export default function Tattoo(props) {
     return(
         <div>
             <h3>{tattoo.description}</h3>
+            {
+                tattoo.tags.map((tag, index)=> {
+                    return <p key={index}>{tag}</p>
+                })
+            }
 
 
 
